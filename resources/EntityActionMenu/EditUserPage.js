8dbe776e74f2d228abc3d29272abc4fa24abb584
pwirth <wirth@hallowelt.biz>
2020-10-01 10:57:58 +0200

    Implement UX review
    ERM:20227
    [3.3]
    
    Change-Id: I83778d38bbfa11f0eaffeb966ded621b4482dab4

---
+++ resources/EntityActionMenu/EditUserPage.js
@@ -0,0 +1,38 @@
+/*
+* @author     Stefan Kühn
+* @package    BluespiceSocial
+* @subpackage BlueSpiceSocial
+* @copyright  Copyright (C) 2020 Hallo Welt! GmbH, All rights reserved.
+* @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
+*/
+
+bs.social = bs.social || {};
+bs.social.EntityActionMenu = bs.social.EntityActionMenu || {};
+
+bs.social.EntityActionMenu.EditUserPage = function ( entityActionMenu ) {
+	OO.EventEmitter.call( this );
+	var me = this;
+	me.entityActionMenu = entityActionMenu;
+	me.$element = null;
+	me.priority = 50;
+	if( me.entityActionMenu.entity.getData().outputtype !== 'Page' ) {
+		return;
+	}
+	me.$element = $( '<li><a class="dropdown-item bs-social-entity-action-edituserpage">'
+		+ '<span>' + mw.message( "bs-social-entityaction-edit" ).plain() + '</span>'
+		+ '</a></li>'
+	);
+	me.$element.on( 'click', function( e ) { me.click( e ); } );
+};
+
+OO.initClass( bs.social.EntityActionMenu.EditUserPage );
+OO.mixinClass( bs.social.EntityActionMenu.EditUserPage, OO.EventEmitter );
+
+bs.social.EntityActionMenu.EditUserPage.prototype.click = function ( e ) {
+	window.location.href = mw.util.getUrl(
+		this.entityActionMenu.entity.data.get( 'relatedtitle' ),
+		{ action:'edit' }
+	);
+	e.preventDefault();
+	return false;
+};
